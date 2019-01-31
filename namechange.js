e062bd0bc446f3c9a5ad9b6a7819899496616eb7
Min Kaung Htet <minkaung.htet@mm-digital-solutions.com>
2019-01-31 23:24:08 +0630

    first_js

---
+++ namechange.js
@@ -0,0 +1,20 @@
+const name = "AlbERt eINstEiN";
+
+    function nameFormatter(oldName) {      
+      /* 
+        Split the string by space
+        Slice the first character and make it uppercase
+        Slice the reset of the characters and make it lowercase
+        Join the splitted array with space to get the final result
+        Final result should be "Albert Einstein"                
+        */
+       var text = "AlbERt eINstEiN";
+        text = text.toLowerCase()
+    .split(' ')
+    .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+    .join(' ');
+
+       
+        return text;
+    }
+    console.log(nameFormatter(name));
